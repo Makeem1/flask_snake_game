e0364e797a7c45e90f28c2b0bc1ed2d43538cb8c
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-01-30 15:53:55 -0500

    Modifying conftest file in test package

--- flask_snake_game/snakeeyes/app.py
+++ flask_snake_game/snakeeyes/app.py
@@ -4,12 +4,18 @@ from snakeeyes.blueprints.page import page
 from snakeeyes.blueprints.contact import contact
 from snakeeyes.extensions import debug_toolbar, mail, Csrf
 
+
+
+""" If the settings_override is defined, the config will be updated to the settings_override parameter, if not, the config settings.py is used. """
+
 def create_app(settings_override = None):
 
 
 	app = Flask(__name__, instance_relative_config = True)
 
-	app.config.from_object('config.settings')
+	"""Go to the folder config which is relative to the app.yp folder and check settings """
+	app.config.from_object('config.settings')  
+	"""Go to the instance folder and search for settings.py is it exist inside the config folder, if not, report silent i.e do not give error   """
 	app.config.from_pyfile('settings.py', silent=True)
 
 	# @app.route('/')
@@ -17,6 +23,9 @@ def create_app(settings_override = None):
 	# 	return app.config['HELLO']
 
 	if settings_override:
+		"""
+			app.config.update() help to update settings.py file in the config file 
+		"""
 		app.config.update(settings_override)
 
 	app.register_blueprint(page)
--- flask_snake_game/snakeeyes/tests/conftest.py
+++ flask_snake_game/snakeeyes/tests/conftest.py
@@ -6,7 +6,6 @@ from snakeeyes.app import create_app
 @pytest.yield_fixture(scope='session')
 def app():
 
-
 	params = {
 		'DEBUG' : False,
 		'TESTING' : True,
@@ -17,12 +16,13 @@ def app():
 	ctx = _app.app_context()
 	ctx.push()
 
+	"""The code above the yield function serve as a set up """
 	yield _app
-
+	"""The code below the yield function serve as a tear down  """
 	ctx.pop()
 
 
 @pytest.yield_fixture(scope='function')
 def client(app):
-	'''Setting up app client, this get executed for each test function '''
+	'''Setting up app client, this get executed for each test function in isolation. test_client() help us to direct a test to a specific route '''
 	yield app.test_client()
