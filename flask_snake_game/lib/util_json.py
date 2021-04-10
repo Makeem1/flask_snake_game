28b7282869926ee107e2ad9ab6afc470ca024375
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-10 23:09:55 +0100

    adding a function that convert coupon response from stripe to json using jsonify

--- flask_snake_game/lib/util_json.py
+++ flask_snake_game/lib/util_json.py
@@ -1,7 +1,15 @@
 from flask import jsonify 
 
 
-def render_json(*args, **kwargs):
+def render_json(status, *args, **kwargs):
 	"""
 	Return a json response 
+	Examaple usage:
+		render_json(404, {'error' : 'Discount code not found'})
+		render_json(200, {'data' : coupon.to_json()})
 	"""
+
+	response = jsonify(*args, **kwargs)
+	response.status_code = status
+
+	return response 
