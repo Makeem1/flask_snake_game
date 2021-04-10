2cc9b712be88531274588cd3adfc8de58ef7bbe5
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-10 23:05:12 +0100

    adding comment

--- flask_snake_game/snakeeyes/blueprints/billing/models/coupon.py
+++ flask_snake_game/snakeeyes/blueprints/billing/models/coupon.py
@@ -136,6 +136,7 @@ class Coupon(ResourceMixin, db.Model):
             payment_params['code'] = payment_params['id']
             del payment_params['id']
 
+        # Converting the UNIX time stamp to a daytime stamp that the database expect 
         if 'redeem_by' in payment_params:
             if payment_params.get('redeem_by') is not None:
                 params['redeem_by'] = payment_params.get('redeem_by').replace(
