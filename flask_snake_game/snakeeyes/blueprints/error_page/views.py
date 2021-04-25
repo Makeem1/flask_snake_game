88a8573bac9be6c9ab1480a0661a9b99a4ba9c88
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-25 23:20:07 +0100

    adding error view function for 429

--- flask_snake_game/snakeeyes/blueprints/error_page/views.py
+++ flask_snake_game/snakeeyes/blueprints/error_page/views.py
@@ -7,5 +7,11 @@ def page_not_found(e):
     return render_template('error/error_404.html'), 404
 
 @error.app_errorhandler(500)
-def ser_error(e):
+def server_error(e):
     return render_template('error/error_500.html'), 500
+
+
+@error.app_errorhandler(429)
+def page_limited(e):
+	return render_template('error/error_429.html'), 429
+	
