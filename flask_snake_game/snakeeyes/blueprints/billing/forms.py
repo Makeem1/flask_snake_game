2a6e672979827cec5c0894133adbc26f14823be5
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-04 23:51:17 +0100

    Adding payment form

--- flask_snake_game/snakeeyes/blueprints/billing/forms.py
+++ flask_snake_game/snakeeyes/blueprints/billing/forms.py
@@ -1,6 +1,23 @@
 from flask_wtf import Form
-from wtforms import StringField, HiddenField
+from wtforms import StringField, HiddenField, SelectField
 from wtforms.validators import DataRequired, Optional, Length
+
+from config.settings import COINS_BUNDLES
+
+def choices_from_coin_bundles():
+    """
+    Convert the COIN_BUNDLE settings into slevt box items.
+
+    :return: list
+    """
+
+    choices = []
+
+    for bundle in COINS_BUNDLES:
+      pair = (str(bundle.get('coins')), bundle.get('label'))
+      choices.append(pair)
+
+    return choices 
 
 
 class CreditCardForm(Form):
@@ -21,3 +38,14 @@ class UpdateSubscriptionForm(Form):
 
 class CancelSubscriptionForm(Form):
     pass
+
+  
+class PaymentForm(Form):
+    stripe_key = HiddenField('Stripe publishable key',
+                             [DataRequired(), Length(1, 254)])
+    coins_bundles = SelectField('How many coins do you want?',
+                       [DataRequired()], choices=choices_from_coin_bundles())
+    coupon_code = StringField('Do you have a coupon code?',
+                              [Optional(), Length(1, 128)])
+    name = StringField('Name on card',
+                       [DataRequired(), Length(1, 254)])
